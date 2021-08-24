3b330eef67123e7030e0fc4ca3b32ab1a46f7e34
chrystalme <afam_ifeanyi@live.com>
2021-08-24 22:17:32 +0300

    Add league proptypes validation

--- src/components/League.jsx
+++ src/components/League.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const League = ({ league }) => {
-  const { competition, season } = league;
+  const { competition, season, teams } = league;
   console.log(league);
 
   return (
     <div>
       <h3>{competition.name}</h3>
       <h3>{season.startDate}</h3>
+      {teams.map((team) => (<h5 key={team.id}>{team.name}</h5>))}
     </div>
   );
 };
@@ -21,6 +22,10 @@ League.propTypes = {
     season: PropTypes.shape({
       startDate: PropTypes.string,
     }),
+    teams: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+    }).isRequired).isRequired,
   }).isRequired,
 };
 
